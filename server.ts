7ce3e245640d802731e899874da188d3590f1d90
Wiktor Płocki <wiktor.plocki@gmail.com>
2020-04-29 13:05:34 +0200

    Refactor server code

--- server.ts
+++ server.ts
@@ -1,9 +1,8 @@
-import jsonServer from 'json-server';
+import jsonServer from "json-server";
 
 const server = jsonServer.create();
-const router = jsonServer.router('./mocks/routes.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
-server.listen(3001, () => console.log('JSON Server running'));
+
+server.listen(3001, () => console.log("JSON Server running"));
